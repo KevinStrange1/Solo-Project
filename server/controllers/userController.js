e9b50cf703e793c83611eb27b41cf34b2b920ebf
Kevin Strange <kevin.strange1000@gmail.com>
2023-06-08 14:55:31 +0100

    feat: getting spotify access token

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,38 @@
 const User = require('../models/User');
+const axios = require('axios');
+const qs = require('qs');
+require('dotenv').config();
+
+let spotifyToken = '';
+
+const refreshSpotifyToken = async () => {
+  try {
+    const url = 'https://accounts.spotify.com/api/token';
+
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization:
+        'Basic ' +
+        Buffer.from(
+          process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET
+        ).toString('base64'),
+    };
+
+    const data = qs.stringify({
+      grant_type: 'client_credentials',
+    });
+
+    const response = await axios.post(url, data, { headers });
+    spotifyToken = response.data.access_token;
+
+    setTimeout(refreshSpotifyToken, (response.data.expires_in - 300) * 1000);
+  } catch (error) {
+    console.error(error);
+    setTimeout(refreshSpotifyToken, 60 * 1000);
+  }
+};
+
+refreshSpotifyToken();
 
 exports.createUser = async (req, res) => {
   const { name, mood, activity } = req.body;
@@ -15,4 +49,8 @@ exports.createUser = async (req, res) => {
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
+};
+
+exports.getSpotifyToken = async (req, res) => {
+  res.json({ access_token: spotifyToken });
 };
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { createUser } = require('./controllers/userController');
+const { createUser, getSpotifyToken } = require('./controllers/userController');
 
 router.post('/', createUser);
+router.get('/spotify-token', getSpotifyToken);
 
 module.exports = router;
