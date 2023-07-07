38db282b5a58d98e7c876bd5bcf2eaa4484d7784
Kevin Strange <kevin.strange1000@gmail.com>
2023-07-07 20:36:10 +0100

    up

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const AUTH_URL =
-  'https://accounts.spotify.com/authorize?client_id=e665f2df3b3c4e19bbc7449267ebff04&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
+  "https://accounts.spotify.com/authorize?client_id=e665f2df3b3c4e19bbc7449267ebff04&response_type=code&redirect_uri=https://groove-guru.vercel.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 function Login() {
   return (
