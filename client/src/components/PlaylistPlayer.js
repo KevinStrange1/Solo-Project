4bd09c51e4e7ad43077696259b76a3c14b35d60a
Kevin Strange <kevin.strange1000@gmail.com>
2023-07-08 08:15:12 +0100

    grr

--- client/src/components/PlaylistPlayer.js
+++ client/src/components/PlaylistPlayer.js
@@ -1,39 +1,25 @@
-import React from 'react';
-import SpotifyPlayer from 'react-spotify-web-playback';
+import React from "react";
+import SpotifyPlayer from "react-spotify-web-playback";
 
 const PlaylistPlayer = ({ playlist, accessToken }) => {
   return (
     <div className="spotify-player">
       <h2>Now Playing: {playlist.name}</h2>
-      <div className="player">
-        <SpotifyPlayer
-          token={accessToken}
-          uris={[playlist.uri]}
-          styles={{
-            activeColor: '#1cb954',
-            bgColor: '#282828',
-            color: '#ffffff',
-            loaderColor: '#1cb954',
-            sliderColor: '#1cb954',
-            trackArtistColor: '#ffffff',
-            trackNameColor: '#ffffff',
-          }}
-        />
-      </div>
+      <SpotifyPlayer
+        token={accessToken}
+        uris={[playlist.uri]}
+        styles={{
+          activeColor: "#1cb954",
+          bgColor: "#282828",
+          color: "#ffffff",
+          loaderColor: "#1cb954",
+          sliderColor: "#1cb954",
+          trackArtistColor: "#ffffff",
+          trackNameColor: "#ffffff",
+        }}
+      />
     </div>
   );
 };
 
 export default PlaylistPlayer;
-
-// return (
-//   <div className="spotify-player">
-//     <h2>Now Playing: {playlist.name}</h2>
-//     <SpotifyPlayer
-//       uri={playlist.uri}
-//       size={{ width: '100%', height: '500' }}
-//       view="list"
-//       theme="black"
-//     />
-//   </div>
-// );
